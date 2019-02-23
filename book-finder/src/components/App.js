67c6d9f63346fe2f6f7dd3c56f6510e35f35e4f0
ZumDeWald <zumdewald@gmail.com>
2019-02-23 12:48:32 -0600

    Initial Search bar setup

--- book-finder/src/components/App.js
+++ book-finder/src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+
+//Components
+import Search from './Search.js';
+
+//Stylesheets
 import '../stylesheets/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          BOOM
-        </header>
+        <Search />
       </div>
     );
   }
