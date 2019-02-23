65b0f7aa58412c9e1cb3f4c3b5095b814e53cf4d
ZumDeWald <zumdewald@gmail.com>
2019-02-23 12:39:45 -0600

    REFACTOR remove CRA generated code

---
+++ book-finder/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          BOOM
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
